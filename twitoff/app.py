d42c226d48be88c7b571d942c41940a5090efe10
Alexander Kim <alex000kim@gmail.com>
2019-12-09 00:30:22 -0500

    update app.py

--- twitoff/app.py
+++ twitoff/app.py
@@ -5,7 +5,7 @@ from .models import DB, User, Tweet
 def create_app():
 	app = Flask(__name__)
 	app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')
-	app.config['ENV'] = config('ENV')
+	app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 	DB.init_app(app)
 
 	@app.route('/')
@@ -18,5 +18,16 @@ def create_app():
 		DB.drop_all()
 		DB.create_all()
 		return render_template('base.html', title='hello')
+
+	@app.route('/iris')
+	def iris():    
+		from sklearn.datasets import load_iris
+		from sklearn.linear_model import LogisticRegression
+		X, y = load_iris(return_X_y=True)
+		clf = LogisticRegression(
+			random_state=0, 
+			solver='lbfgs', 
+			multi_class='multinomial').fit(X, y)
+		return str(clf.predict(X[:2, :]))
 
 	return app
