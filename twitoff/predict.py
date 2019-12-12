b7179fba9dc7aa82dd51ad2b5b427c075998105c
Alexander Kim <alex000kim@gmail.com>
2019-12-11 22:58:27 -0500

    replace logistic regression with nearest neighbour search

--- twitoff/predict.py
+++ twitoff/predict.py
@@ -1,7 +1,6 @@
 """Prediction of Users based on Tweet embeddings."""
 import numpy as np
-from sklearn.linear_model import LogisticRegression
-
+from sklearn.neighbors import NearestNeighbors
 from .models import User
 from .twitter import BASILICA
 
@@ -15,15 +14,19 @@ def predict_user(user1_name, user2_name, tweet_text):
         tweet_text: str, tweet text to evaluate
 
     # Returns
-        prediction from logistic regression model
+        True if user1 has at least one tweet that is closer (measured in cosine similarity) to the sample tweet.
+        False otherwise
     """
     user1 = User.query.filter(User.name == user1_name).one()
     user2 = User.query.filter(User.name == user2_name).one()
     user1_embeddings = np.array([tweet.embedding for tweet in user1.tweets])
     user2_embeddings = np.array([tweet.embedding for tweet in user2.tweets])
-    embeddings = np.vstack([user1_embeddings, user2_embeddings])
-    labels = np.concatenate([np.ones(len(user1.tweets)),
-                             np.zeros(len(user2.tweets))])
-    log_reg = LogisticRegression().fit(embeddings, labels)
+    user1_neigh = NearestNeighbors(metric='cosine').fit(user1_embeddings)
+    user2_neigh = NearestNeighbors(metric='cosine').fit(user2_embeddings)
     tweet_embedding = BASILICA.embed_sentence(tweet_text, model='twitter')
-    return log_reg.predict(np.array(tweet_embedding).reshape(1, -1))
+    tweet_embedding = np.array(tweet_embedding).reshape(1, -1)
+    user_1_neigh_dist, _ = user1_neigh.kneighbors(X=tweet_embedding, n_neighbors=1)
+    user_2_neigh_dist, _ = user2_neigh.kneighbors(X=tweet_embedding, n_neighbors=1)
+    user_1_neigh_dist = user_1_neigh_dist[0][0]
+    user_2_neigh_dist = user_2_neigh_dist[0][0]
+    return user_1_neigh_dist > user_2_neigh_dist
